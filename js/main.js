7bc2e1b96308de2a572cde4e8e337ea99733952a
ajvidal91 <resilientcodersogmac@Resilients-MacBook-Pro.local>
2018-05-24 19:18:34 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const mainPhotoArray = ['images/store-front.jpg', 'images/store-front.jpg', 'images/shake-chocolate.jpeg']
+const frontPageCarousel = $('.carousel-first-page')
+
+
+frontPageCarousel.slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
